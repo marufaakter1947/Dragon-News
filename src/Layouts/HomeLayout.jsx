d0eaa4b4c4306daaaf84a615ef1b6b4b89a65287
Marufa Akter <marufaakter1947@gmail.com>
2025-10-18 00:31:31 +0600

    Added Navbar design

--- src/Layouts/HomeLayout.jsx
+++ src/Layouts/HomeLayout.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Outlet } from 'react-router';
 import Header from '../Components/Header';
 import LatestNews from '../Components/LatestNews';
+import Navbar from '../Components/Navbar';
 
 const HomeLayout = () => {
     return (
@@ -11,6 +12,11 @@ const HomeLayout = () => {
                 <section className='w-11/12 mx-auto my-3'>
                     <LatestNews></LatestNews>
                 </section>
+                <nav className='w-11/12 mx-auto my-3'>
+                    <Navbar>
+
+                    </Navbar>
+                </nav>
             </header>
             <main>
             
