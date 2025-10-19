ec58e66a8d07c4fa7f530743bf3831ed52de40a3
Marufa Akter <marufaakter1947@gmail.com>
2025-10-19 08:03:13 +0600

    Register user and onAuthStateChange

---
+++ src/Firebase/Firebase.config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyA-eS0QsT-HL-DzEvXgx_ETltAXQxbJMIM",
+  authDomain: "dragon-news-5b93e.firebaseapp.com",
+  projectId: "dragon-news-5b93e",
+  storageBucket: "dragon-news-5b93e.firebasestorage.app",
+  messagingSenderId: "428334398413",
+  appId: "1:428334398413:web:154fa3f9ff53a349406284"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
